0af1f8fc2f12e9dd90d9523c8977ea6341dc1771
abc <abc@gmail.com>
2024-07-01 08:18:41 +0700

    first commit

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from "next/link";
+
+const Dashboard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ app/menu/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from "next/link";
+
+const Menu = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Menu
